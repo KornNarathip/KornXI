f94f22690018c121651fddc96adeb968933b9d1c
KornNarathip <90178286+KornNarathip@users.noreply.github.com>
2021-09-27 06:22:03 +0700

    Add files via upload

--- js/im.js
+++ js/im.js
@@ -1,14 +1,25 @@
-// typing text animation script
-var typed = new Typed(".typing", {
-    strings: ["YouTuber", "Developer", "Student", "Cyber Security",],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
-});
+$(document).ready(function(){
+    $(window).scroll(function(){
+        // sticky navbar on scroll script
+        if(this.scrollY > 20){
+            $('.navbar').addClass("sticky");
+        }else{
+            $('.navbar').removeClass("sticky");
+        }
+        
+        // scroll-up button show/hide script
+        if(this.scrollY > 500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
+        }
+    });
 
-var typed = new Typed(".typing-2", {
-    strings: ["YouTuber", "Developer", "Student", "Cyber security",],
-    typeSpeed: 100,
-    backSpeed: 60,
-    loop: true
+    var typed = new Typed(".typing-2", {
+        strings: ["YouTuber", "Developer", "Student", "Cyber security",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
 });
