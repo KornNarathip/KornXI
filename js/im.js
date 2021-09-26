e5629254062d11b24abb44e79725489671a48774
KornNarathip <90178286+KornNarathip@users.noreply.github.com>
2021-09-26 16:18:46 +0700

    Add files via upload

---
+++ js/im.js
@@ -0,0 +1,14 @@
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["YouTuber", "Developer", "Student", "Cyber Security",],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["YouTuber", "Developer", "Student", "Cyber security",],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
